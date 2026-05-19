d560a3d2e3c8f48e28bf785246e80131ff4608f8
Anthony <cristo045millanperez@gmail.com>
2026-05-19 14:39:02 -0600

    Versión de PWA 4.1

--- pwab/sw.js
+++ pwab/sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "4.03"
+const VERSION = "4.1"
 
 /** Nombre del archivo de cache. */
 const CACHE = "ByteForcePWA"
