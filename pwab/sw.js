e1cc3242b57226ac2d942fa8b19e738c5932cae3
Anthony <cristo045millanperez@gmail.com>
2026-05-20 00:19:37 -0600

    img cambiadas

--- pwab/sw.js
+++ pwab/sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "4.2"
+const VERSION = "4.02"
 
 /** Nombre del archivo de cache. */
 const CACHE = "ByteForcePWA"
